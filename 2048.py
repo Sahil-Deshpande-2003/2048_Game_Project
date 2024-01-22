8f2aed633b8a8ad1e93118f8a2559921ef3a765c
Sahil-Deshpande-2003 <123302700+Sahil-Deshpande-2003@users.noreply.github.com>
2024-01-22 10:00:40 +0530

    Added some comments in 2048.py, added my handwritten notes and info.txt

--- 2048.py
+++ 2048.py
@@ -31,7 +31,11 @@ class Game2048(Frame):
 # that object
         self.grid()
 # Initializes the grid manager for the frame, allowing widgets to be organized in a grid layout.
+
         '''
+        The Grid geometry manager puts the widgets in a 2-dimensional table. The master widget is split into a number of rows and columns, and each “cell” in the resulting table can hold a widget. 
+
+
         A grid is a visual layout or structure used in graphical user interfaces (GUIs) to organize and position graphical elements (widgets)
         such as buttons, labels, and text fields.
         It is primarily used for the presentation and arrangement of GUI components within a window or frame.
@@ -58,7 +62,7 @@ class Game2048(Frame):
                         }
 
 
-# if w is pressed move up, s is pressed move down, a for left and d for right -> check how to use arrows instead!
+# if w is pressed move up, s is pressed move down, a for left and d for right 
 
     # Initializes an empty list self.grid_cells to store references to the GUI grid cells.    
        
@@ -124,13 +128,14 @@ class Game2048(Frame):
 # needs to be changed, this is done by update_grid_cells, so it sets the 
 # colour according to numbers 
 
-        self.mainloop()
+        self.mainloop() # isse event loop me chale jate hai, ye aapko GUI window me rakhta hai
 
 # Starts the Tkinter main event loop, which runs the game until the user closes the window
 
 # it actually runs the program
 
     def init_grid(self):
+        # By passing self as the first argument, we are essentially saying this frame (background) is a part of the current Game2048 object
         background = Frame(self, bg=c.BACKGROUND_COLOR_GAME,
                            width=c.SIZE, height=c.SIZE)
 # frame of 400 x 400 created inside the outside frame 
@@ -140,6 +145,8 @@ class Game2048(Frame):
 
         background.grid()
 
+# The Grid geometry manager puts the widgets in a 2-dimensional table. The master widget is split into a number of rows and columns, and each “cell” in the resulting table can hold a widget. This method is used to organize widgets in a table-like structure, arranging them in rows and columns.
+
         for i in range(c.GRID_LEN):
             grid_row = []
             for j in range(c.GRID_LEN):
@@ -147,17 +154,27 @@ class Game2048(Frame):
                              width=c.SIZE / c.GRID_LEN,# so width=400/4=100
                              height=c.SIZE / c.GRID_LEN)
                 
-                # cell is itself is a frame
+                # cell is itself is a frame, cell represents a cell in the grid.
+                '''
+                This line uses the grid() method to position and display the cell frame within the grid layout. The parameters are:
 
+                row=i: Specifies the row in the grid where the cell frame should be placed. The value of i is used as the row index.
+                column=j: Specifies the column in the grid where the cell frame should be placed. The value of j is used as the column index.
+                padx=c.GRID_PADDING: Specifies horizontal padding (in pixels) around the cell frame.
+                pady=c.GRID_PADDING: Specifies vertical padding (in pixels) around the cell frame.
+                '''
                 cell.grid(row=i, column=j, padx=c.GRID_PADDING,
                           pady=c.GRID_PADDING)
         
 # adding the cell to the grid at row = i and column = j
+                
+# Tkinter Label is a widget that is used to implement display boxes where you can place text or images.
 
                 t = Label(master=cell, text="",
                           bg=c.BACKGROUND_COLOR_CELL_EMPTY,
                           justify=CENTER, font=c.FONT, width=5, height=2)
-
+# master=cell: Specifies the parent widget for the label. In this case, it's the cell frame, indicating that the label (t) is a child of the cell frame.
+# text="": Sets the initial text content of the label to an empty string
 # inside the cell, you are adding a label, label is another widget which is used
 # to denote a text box, since this cell was a grid, so label is also a grid
 #   justify=CENTER means adding text as center,
@@ -174,17 +191,24 @@ class Game2048(Frame):
 
 # inside a cell, there is only label so label covers the cell, if we change background colour
 # of label so automatically cell's background colour will change  
-                t.grid()
-                grid_row.append(t)
+                t.grid() # # The Grid geometry manager puts the widgets in a 2-dimensional table. The master widget is split into a number of rows and columns, and each “cell” in the resulting table can hold a widget. This method is used to organize widgets in a table-like structure, arranging them in rows and columns.
 
-            self.grid_cells.append(grid_row)
+                # t.grid(row=0, column=0)  # Place the label in the first row and first column of the grid
+
+                #  If you don't specify row and column values explicitly, it will automatically position the widget in the first available row and column.
+
+                # so here create a label 't' and insert in the 1st available row and column in the grid
+
+                grid_row.append(t) # grid_row is a list that represents a single row in the grid.
+
+            self.grid_cells.append(grid_row) # self.grid_cells is a list that represents the entire grid in the game.
 
 
     def init_matrix(self):
 
 # all the blocks (i.e. L1,L2,L3,L4,...) get created
 
-        self.matrix = logicsfinal.start_game()
+        self.matrix = logicsfinal.start_game() # append 4 lists in a list in which each list has 4 0's
 # above line tells us that if "w" is pressed you have to move up and since  LogicsFinal
 # needs a matrix so I am passing self.matrix as argument in key_down function
 
@@ -193,7 +217,7 @@ class Game2048(Frame):
 # another matrix, and all the changes made in this matrix are reflected in this UI (i . e.
 # L1,L2,.. wala matrix) 
 
-        logicsfinal.add_new_2(self.matrix)
+        logicsfinal.add_new_2(self.matrix) # insert a new 2 at random position provided no other number is present there i.e. 0 is present
 # here we have added a new 2
         logicsfinal.add_new_2(self.matrix)
 # here again we have added a new 2
@@ -204,7 +228,7 @@ class Game2048(Frame):
                 new_number = self.matrix[i][j]
                 if new_number == 0:
                     self.grid_cells[i][j].configure(
-                        text="", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+                        text="", bg=c.BACKGROUND_COLOR_CELL_EMPTY) # by default all the values in the grid are 0
 # as for 0, text should be empty and background color shoudl be this 
 #  BACKGROUND_COLOR_CELL_EMPTY, so configure will do that 
                 else:
@@ -235,13 +259,13 @@ class Game2048(Frame):
 
 # self.commands contains "w","s","a","d", so if any one of them is pressed then only
 # we consider
-            self.matrix, changed = self.commands[repr(event.char)](self.matrix)
+            self.matrix, changed = self.commands[repr(event.char)](self.matrix) # self.commands[repr(event.char)] will return logicsfinal.move_up or down or right or left and to call the respective function which is present in logicsfinal.py, you need to pass the grid as argument which is self.matrix here
             if changed:
                 logicsfinal.add_new_2(self.matrix)
 
 # if there is any change then only you need to add a 2
 
-                self.update_grid_cells()
+                self.update_grid_cells() # configure the [text,bg and fg(color of each number)](of each number) for each number in this updated grid
 
 # update_grid_cells changes the UI cause a new 2 is added 
 
